2ce674d3d9019d7fe028eada66d9bbb597f4d6c4
Burak Ugar <burak.ugar@getmanta.io>
2024-03-04 21:12:53 +0100

    the repo is initialized

---
+++ src/test/java/com/house/hunter/HouseHunterBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.house.hunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseHunterBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
